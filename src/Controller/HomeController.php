513a023fbb57a1ce6317e747149fa35736c6a2d6
Maxime Guichard <maxime.guichard45@gmail.com>
2021-07-22 16:24:14 +0200

    création des derniers dans home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SeatRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(SeatRepository $seatRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'seats' => $seatRepository->findBy(
+                [],
+                ['id' => 'DESC'],
+                3
+            )
+        ]);
     }
 }
